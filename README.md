0ea4c9a5b2ec6b2d2996bbfb51cecde0babc68eb
Zheng Chang <zhengchang@microsoft.com>
2021-09-12 16:42:26 +0800

    Complete first iteration

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 * Erase the panda face
 * Export/Download, make sure the picture size is 24 * 24, and in .png
 * Put the picture under related catagory based on your own understanding
+
+# What is Hello Panda project
+Inspired by CryptoPunk project, we created 540 unique collectible pandas as first iteration with proof of ownership stored on the Ethereum blockchain. This is an indivial project and my goal is to keep creating unique pandas with the unique people from all over the world.
+
+# How to contribute to the project
+I'm still working on an open standard that allow everyone contribute to this project by providing anything they like to the 24px*24px pandas.
--- hello.py
+++ hello.py
@@ -1,30 +1,68 @@
 from PIL import Image
-import cv2
-import numpy
 import glob
+from pathlib import Path
 
-folders = glob.glob('/home/zhengchang/workspace/helloPanda/accessories')
-imagenames_list = []
-for folder in folders:
-    for f in glob.glob(folder+'/*.jpg'):
-        imagenames_list.append(f)
+## load face
+face_images = []        
+for f in glob.glob('./face/*.png'):
+    face_images.append(Image.open(f))
 
-read_images = []        
-for image in imagenames_list:
-    read_images.append(cv2.imread(image, cv2.IMREAD_GRAYSCALE))
+## load accessories
+accessories_images = []        
+for f in glob.glob('./accessories/*.png'):
+    accessories_images.append(Image.open(f))
 
-print(read_images)
+## load ear
+ear_images = []        
+for f in glob.glob('./ear/*.png'):
+    ear_images.append(Image.open(f))
 
-# msg = "Hello World"
+## load eye
+eye_images = []        
+for f in glob.glob('./eye/*.png'):
+    eye_images.append(Image.open(f))
 
-# face = Image.open('./face/tpanda.png')
-# hat = Image.open('./hat/colorful.png')
-# mouth = Image.open('./mouth/bamboo.png')
-# background = Image. new('RGB', (100, 30), color = (73, 109, 137))
+## load head
+head_images = []        
+for f in glob.glob('./head/*.png'):
+    head_images.append(Image.open(f))
 
-# background.paste(face, (38, 3), face)
-# background.paste(hat, (38, 3), hat)
-# background.paste(mouth, (38, 3), mouth)
+## load mouth
+mouth_images = []        
+for f in glob.glob('./mouth/*.png'):
+    mouth_images.append(Image.open(f))
 
-# background.save('./target/test.png')
-# print(msg)
+print("Hello Panda")
+
+for face in face_images: 
+    for head in head_images: 
+        for mouth in mouth_images: 
+            for accessory in accessories_images: 
+                background = Image.new('RGBA', (24, 24), (255, 0, 0, 0))
+                background.paste(face, (0, 0), face)
+                background.paste(head, (0, 0), head)
+                background.paste(mouth, (0, 0), mouth)
+                background.paste(accessory, (0, 0), accessory)
+                background.save('./target/test.png')
+                facefilename = Path(str(face.filename)).stem
+                headfilename = Path(str(head.filename)).stem
+                if headfilename == 'empty':
+                    headfilename = ''
+                else:
+                    headfilename = ' with ' + headfilename
+                mouthfilename = Path(str(mouth.filename)).stem
+                if mouthfilename == 'empty':
+                    mouthfilename = ''
+                elif headfilename == '':
+                    mouthfilename = ' with ' + mouthfilename
+                else:
+                    mouthfilename = ' and ' + mouthfilename
+                accessoryfilename = Path(str(accessory.filename)).stem
+                if accessoryfilename == 'empty':
+                    accessoryfilename = ''
+                elif ((headfilename == '') and (mouthfilename == '')):
+                    accessoryfilename = ' with ' + accessoryfilename
+                else:
+                    accessoryfilename = ' and ' + accessoryfilename
+                filename =  facefilename + headfilename + mouthfilename + accessoryfilename
+                background.save('./target/' + filename + '.png')
