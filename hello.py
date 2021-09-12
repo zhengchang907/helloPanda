41cd717ce71f5a88ac3abdebd838f24a143227ac
zhengchang907 <zhengchang@microsoft.com>
2021-09-12 13:15:33 +0800

    Fillin directories

--- hello.py
+++ hello.py
@@ -1,15 +1,30 @@
 from PIL import Image
+import cv2
+import numpy
+import glob
 
-msg = "Hello World"
+folders = glob.glob('/home/zhengchang/workspace/helloPanda/accessories')
+imagenames_list = []
+for folder in folders:
+    for f in glob.glob(folder+'/*.jpg'):
+        imagenames_list.append(f)
 
-face = Image.open('./face/tpanda.png')
-hat = Image.open('./hat/colorful.png')
-mouth = Image.open('./mouth/bamboo.png')
-background = Image. new('RGB', (100, 30), color = (73, 109, 137))
+read_images = []        
+for image in imagenames_list:
+    read_images.append(cv2.imread(image, cv2.IMREAD_GRAYSCALE))
 
-background.paste(face, (38, 3), face)
-background.paste(hat, (38, 3), hat)
-background.paste(mouth, (38, 3), mouth)
+print(read_images)
 
-background.save('./target/test.png')
-print(msg)
+# msg = "Hello World"
+
+# face = Image.open('./face/tpanda.png')
+# hat = Image.open('./hat/colorful.png')
+# mouth = Image.open('./mouth/bamboo.png')
+# background = Image. new('RGB', (100, 30), color = (73, 109, 137))
+
+# background.paste(face, (38, 3), face)
+# background.paste(hat, (38, 3), hat)
+# background.paste(mouth, (38, 3), mouth)
+
+# background.save('./target/test.png')
+# print(msg)
